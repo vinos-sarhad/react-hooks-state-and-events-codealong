693d09ae34c3cbf9baa52af40c8838f6047409a9
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-03-17 21:37:09 +0300

    done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const[isOn,setIsOn]=useState(false)
+
+  function Turn(){ 
+    
+    setIsOn(!isOn)
+    console.log(isOn)
+  }
+  const color = isOn ? "red" : "white";
+  return <button style={{background:color}} onClick={Turn}> {isOn ? "ON": "OFF"}</button>;
 }
 
 export default Toggle;
